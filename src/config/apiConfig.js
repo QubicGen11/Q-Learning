11b420b81b6ab8d34db7157f96d7b073c4fbb8b2
shaiksajidhussain <sanjusazid0@gmail.com>
2024-11-16 23:32:47 +0530

    deployed url

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -5,7 +5,7 @@ const config = {
     DEPLOY_URL: 'https://dev-be.qubinest.com',
   
     // Current URL - change this line to switch between environments
-    CURRENT_URL: 'http://localhost:8089'  // or 'https://dev-be.qubinest.com'
+    CURRENT_URL: 'https://dev-be.qubinest.com'  // or 'https://dev-be.qubinest.com'
   };
   
   export default config;
