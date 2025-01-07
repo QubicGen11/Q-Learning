877c0da1f20cda8290f71525220603af2238682d
shaiksajidhussain <sanjusazid0@gmail.com>
2025-01-07 13:32:17 +0530

    Adddd the codee

--- src/Components/Instructor Module/Courses/CreateCourse/CourseContent.jsx
+++ src/Components/Instructor Module/Courses/CreateCourse/CourseContent.jsx
@@ -1153,14 +1153,6 @@ function CourseContent() {
         </div>
       </div>
 
-      <div className="mt-6 flex justify-end">
-        <button
-          onClick={handleSave}
-          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
-        >
-          Save Course
-        </button>
-      </div>
 
       {previewOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
