ee37e7fab9c54a9bdda7711669f409bdb75b9795
shaiksajidhussain <sanjusazid0@gmail.com>
2025-01-28 20:55:54 +0530

    Added the after logout page integrations

--- src/Components/When User Logout/Whenuserlogout.jsx
+++ src/Components/When User Logout/Whenuserlogout.jsx
@@ -1,22 +1,41 @@
-import React from 'react'
-import Newnavbar from '../New Landingpage/New Navbar Components/Newnavbar'
-import Latestdeals from '../New Landingpage/LatestDeals Componnets/Latestdeals'
-import Logoutherosec from './Logoutherosec'
-import Logoutskillsforyou from './Logoutskillsforyou'
-import LearnersChoice from '../New Landingpage/Learners Choice/LearnersChoice'
-import Footer from '../New Landingpage/Footer/Footer'
+import { useEffect } from 'react';
+import usePreLoginFeedStore from '../../stores/preLoginFeedStore';
+import Latestdeals from '../New Landingpage/LatestDeals Componnets/Latestdeals';
+import Newnavbar from '../New Landingpage/New Navbar Components/Newnavbar';
+import LearnersChoice from '../New Landingpage/Learners Choice/LearnersChoice';
 
+import Logoutskillsforyou from './Logoutskillsforyou';
+import Footer from '../New Landingpage/Footer/Footer';
+import Logoutherosec from './Logoutherosec';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 const Whenuserlogout = () => {
+  const { learnersChoice, isLoading, error, fetchPreLoginFeed } = usePreLoginFeedStore();
+
+  useEffect(() => {
+    fetchPreLoginFeed(); // Ensure data is fetched
+  }, [fetchPreLoginFeed]);
+
+  if (isLoading) {
+    return <Skeleton height={40} count={5} style={{ marginBottom: '10px' }}  />;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  console.log("LearnersChoice data in Whenuserlogout:", learnersChoice);
+
   return (
     <div className='overflow-hidden'>
-      <Latestdeals/>
-        <Newnavbar/>
-        <Logoutherosec/>
-        <Logoutskillsforyou/>
-        <LearnersChoice/>
-      <Footer/>
+      <Latestdeals />
+      <Newnavbar />
+      <Logoutherosec />
+      <Logoutskillsforyou />
+      <LearnersChoice courses={learnersChoice} /> {/* Pass learnersChoice */}
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Whenuserlogout
+export default Whenuserlogout;
